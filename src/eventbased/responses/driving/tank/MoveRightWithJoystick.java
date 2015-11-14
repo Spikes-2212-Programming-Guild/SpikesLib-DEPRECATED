078fccd8f34b22614fcbe984f4873ee1c495afcc
ThinkRedstone <ittai11@gmail.com>
2015-11-14 13:29:56 +0200

    Added Responses to control a tank driving system

---
+++ src/eventbased/responses/driving/tank/MoveRightWithJoystick.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventbased.responses.driving.tank;
+
+import driving.Tank;
+import edu.wpi.first.wpilibj.Joystick;
+import eventbased.responses.Response;
+
+/**
+ *
+ * @author thinkredstone
+ */
+public class MoveRightWithJoystick extends Response {
+
+    private Tank tank;
+    private Joystick joystick;
+
+    public MoveRightWithJoystick(Tank tank, Joystick joystick) {
+        this.tank = tank;
+        this.joystick = joystick;
+    }
+
+    @Override
+    protected void respond() {
+        tank.setRight(joystick.getY());
+    }
+
+}
