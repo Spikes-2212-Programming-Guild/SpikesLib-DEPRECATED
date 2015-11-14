8f9394e1b59480969e3c10c07014d67d999a1660
ThinkRedstone <ittai11@gmail.com>
2015-11-14 10:22:05 +0200

    Added TankDriveTrain

---
+++ src/driving/TankDriveTrain.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driving;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+/**
+ *
+ * @author thinkredstone
+ */
+public class TankDriveTrain<SC extends SpeedController> implements Tank {
+
+    private Gearbox<SC> left, right;
+
+    public TankDriveTrain(Gearbox<SC> left, Gearbox<SC> right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public TankDriveTrain(SC leftFront, SC leftRear, SC rightFront, SC rightRear) {
+        left = new Gearbox<SC>(leftFront, leftRear);
+        right = new Gearbox<SC>(rightFront, rightRear);
+    }
+
+    @Override
+    public void setRight(double speed) {
+        right.set(speed);
+    }
+
+    @Override
+    public void setLeft(double speed) {
+        left.set(speed);
+    }
+
+    public void forward(double speed) {
+        left.set(-speed);
+        right.set(speed);
+    }
+
+    public void turn(double speed) {
+        left.set(speed);
+        right.set(speed);
+    }
+
+}
