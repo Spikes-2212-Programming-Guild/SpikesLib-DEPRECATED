6f1f77817312a6150ea674d46a48c3747c4cbec0
ThinkRedstone <ittai11@gmail.com>
2015-11-14 09:51:35 +0200

    Added interface for Tank-based driving systems

---
+++ src/driving/Tank.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driving;
+
+/**
+ *
+ * @author thinkredstone
+ */
+public interface Tank {
+    
+    public void setRight();
+    public void setLeft();
+    
+}
