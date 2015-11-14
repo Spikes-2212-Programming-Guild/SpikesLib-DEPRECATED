6b67374b3e241b643997a434d8cdd4539f998a98
ThinkRedstone <ittai11@gmail.com>
2015-11-14 11:35:02 +0200

    Moved Response to responses

--- src/eventbased/responses/Response.java
+++ src/eventbased/responses/Response.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package eventbased;
+package eventbased.responses;
 
 import eventbased.events.Event;
 
