3a859db85d5c92adcdd67ac7386c52d8a5544621
ThinkRedstone <ittai11@gmail.com>
2015-11-14 14:44:16 +0200

    Added code to test driving with teddy

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -4,11 +4,17 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
 
-
+import driving.Tank;
+import driving.TankDriveTrain;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import eventbased.Scheduler;
+import eventbased.events.oi.JoystickYOverThreshold;
+import eventbased.responses.driving.tank.MoveLeftWithJoystick;
+import eventbased.responses.driving.tank.MoveRightWithJoystick;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,17 +24,27 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class RobotTemplate extends IterativeRobot {
+
+    public Scheduler scheduler = new Scheduler();
+    public Joystick left = new Joystick(0);
+    public Joystick right = new Joystick(1);
+    public Tank drivetrain = new TankDriveTrain<Talon>(new Talon(1), new Talon(3), new Talon(2), new Talon(4));
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
+    @Override
     public void robotInit() {
+        scheduler.addResponse(new JoystickYOverThreshold(left, 0.1), new MoveLeftWithJoystick(drivetrain, left));
+        scheduler.addResponse(new JoystickYOverThreshold(right, 0.1), new MoveRightWithJoystick(drivetrain, right));
 
     }
 
     /**
      * This function is called periodically during autonomous
      */
+    @Override
     public void autonomousPeriodic() {
 
     }
@@ -36,15 +52,17 @@ public class RobotTemplate extends IterativeRobot {
     /**
      * This function is called periodically during operator control
      */
+    @Override
     public void teleopPeriodic() {
-        
+        scheduler.run();
     }
-    
+
     /**
      * This function is called periodically during test mode
      */
+    @Override
     public void testPeriodic() {
-    
+
     }
-    
+
 }
