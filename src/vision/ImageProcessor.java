f08ab3b31e3fc065cd6c3ec174063e5df0ccf660
ThinkRedstone <ittai11@gmail.com>
2015-11-24 18:14:42 +0200

    Added ImageProcessor

---
+++ src/vision/ImageProcessor.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vision;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author thinkredstone
+ */
+public class ImageProcessor implements Closeable{
+
+    private Process process = null;
+    private Scanner scanner;
+
+    public ImageProcessor(String path) {
+        try {
+            process = Runtime.getRuntime().exec(path);
+        } catch (IOException ex) {
+            Logger.getLogger(ImageProcessor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        scanner = new Scanner(process.getInputStream());
+    }
+
+    public double getLastMeasurement() {
+        while(scanner.hasNext()){
+            double d = scanner.nextDouble();
+            if(!scanner.hasNext())
+                return d;
+        }
+        return 0;
+    }
+
+    @Override
+    public void close() throws IOException {
+        OutputStream os = process.getOutputStream();
+        os.write(3);//3 is the value of ctrl-c in ascii
+        os.flush();
+    }
+
+}
