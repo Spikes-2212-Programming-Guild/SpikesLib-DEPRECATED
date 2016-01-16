9ee7a7ae9bd076934087457bf47b9b04c3a70d00
ThinkRedstone <ittai11@gmail.com>
2016-01-16 15:05:17 +0200

    Changed TankDrive back to (double,double)

--- src/com/spikes2212/drive/TankDrive.java
+++ src/com/spikes2212/drive/TankDrive.java
@@ -2,8 +2,6 @@ package com.spikes2212.drive;
 
 public interface TankDrive {
 
-	void setLeft(double speed);
-
-	void setRight(double speed);
+	void tankSet(double leftSpeed, double rightSpeed);
 
 }
--- src/com/spikes2212/robot/DriveTrain.java
+++ src/com/spikes2212/robot/DriveTrain.java
@@ -1,26 +1,23 @@
 package com.spikes2212.robot;
 
-import com.spikes2212.controller.MotorController;
+import com.spikes2212.controller.PIDController;
 import com.spikes2212.drive.TankDrive;
 
-public class DriveTrain implements TankDrive {
-	private MotorController left, right;
+public class DriveTrain implements TankDrive{
+	private PIDController left, right;
+        
 
-	public DriveTrain(MotorController left, MotorController right) {
+	public DriveTrain(PIDController left, PIDController right) {
 		this.left = left;
-		this.right = right;
+		this.right = right;                
+                
 	}
 
-	@Override
-	public void setLeft(double speed) {
-		left.set(speed);
+    @Override
+    public void tankSet(double leftSpeed, double rightSpeed) {
+        left.set(leftSpeed);
+        right.set(rightSpeed);
+    }
 
-	}
-
-	@Override
-	public void setRight(double speed) {
-		right.set(speed);
-
-	}
 
 }
